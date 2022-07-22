8467ef8ac6d7f8562bc7d1b4bd2315c127050327
ahmedazzam991 <109697571+ahmedazzam991@users.noreply.github.com>
2022-07-22 19:59:30 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,18 +1,6 @@
 node default {
-  file { '/root/README':
-    ensure => file,
-    content => 'This is a readme',
-    owner => 'root' ,
-  }
 }
-node 'master.puppet.vm' {
+node 'master_puppet_vm' {
   include role::master_server
 }
 
-node /^web/ {
-  include role::app_server
-}
-
-node /^db/ {
-  include role::db_server
-}
